e75354f4543dd48071b1faf583d0812063a4d3a6
Ilya Bazhenov <31971067+bazhil@users.noreply.github.com>
2018-03-30 22:09:41 +0300

    Add files via upload

---
+++ netology_js_hw_1.js
@@ -0,0 +1,29 @@
+'use strict'
+
+//задача 1
+var name;
+var retailPrice;
+
+//задача 2
+name = '«Телепорт бытовой VZHIH-101»';
+retailPrice = 10000;
+
+console.log(`В наличии имеется: ${name}`);
+console.log(`Стоимость товара ${retailPrice} Q`);
+
+//задача 3
+var number = 2;
+var discount = 10;
+console.log('При покупке более двух едениц товара предоставляется скидка 10%');
+console.log('Покупатель хочет приобрести два бытовых телепора, вычисляем счет:');
+var bill = number * retailPrice * ((100 - discount) / 100);
+console.log(`Стоимость двух бытовых телепортов составит ${bill} Q`);
+console.log('Спасибо, что выбираете бытовые телепорты отечественного производства!');
+
+//задача 4
+var balance = 52334224;
+var wholesalePrice = 6500;
+
+var accountBalance = balance % wholesalePrice;
+var count = (balance - accountBalance) / wholesalePrice;
+console.log(`Мы можем закупить ${count} едениц товара, после закупки на счету останется ${accountBalance} Q`);
